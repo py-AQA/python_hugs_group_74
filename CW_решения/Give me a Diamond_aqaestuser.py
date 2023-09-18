e3e9d8ed620dab376c4f5a8d0a1412c067500642
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-09-18 19:44:22 +0200

    решил на листочке )), оказалось разница только в цикле for . Решил добавить заодно и поучиться в гите.

--- CW_решения/Give me a Diamond_aqaestuser.py
+++ CW_решения/Give me a Diamond_aqaestuser.py
@@ -24,3 +24,22 @@ assert diamond(-1) is None, "не то!"
 # отображение красоты ради
 for size in range(3, 12, 2):
     print(diamond(size))
+
+# через for
+def diamond2(n):
+    if n % 2 == 0 or n <= 0:
+        return None
+    lst = ['*' * n]
+    for i in range(n-2, 0, -2):
+        str = ' ' * (int((n - i) / 2)) + '*' * i
+        lst.insert(0, str)
+        lst.append(str)
+    # str = '\n'.join(lst) + '\n'
+    # возвращаем строку собранную через \n из списка lst
+    return '\n'.join(lst) + '\n'
+
+# кусочек проверки с кодварс
+assert diamond2(5) == "  *\n ***\n*****\n ***\n  *\n", "не то!"
+assert diamond2(0) is None, "не то!"
+assert diamond2(2) is None, "не то!"
+assert diamond2(-1) is None, "не то!"
