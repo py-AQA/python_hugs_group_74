a29341a9f947fc7d36d31c1fc032e9d1ee07ad0c
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-09-19 17:08:38 +0300

    решения HW5 01 - 05

--- mike_hw/05/01 Building blocks.py
+++ mike_hw/05/01 Building blocks.py
@@ -1,4 +1,5 @@
-https://www.codewars.com/kata/55b75fcf67e558d3750000a3
+# https://www.codewars.com/kata/55b75fcf67e558d3750000a3
+from helper import test
 
 
 class Block:
@@ -20,3 +21,11 @@ class Block:
     def get_surface_area(self):
         return 2 * (self.height * self.length + self.height * self.width + self.width * self.length)
 
+
+# проверка
+block1 = Block([2, 2, 2])
+test.assert_equals(block1.get_width(), 2)
+test.assert_equals(block1.get_length(), 2)
+test.assert_equals(block1.get_height(), 2)
+test.assert_equals(block1.get_volume(), 8)
+test.assert_equals(block1.get_surface_area(), 24)
