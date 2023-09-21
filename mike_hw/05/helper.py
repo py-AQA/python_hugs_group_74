9cd21567905029039a7aec9e5f0c731fb362021f
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-09-21 23:11:33 +0300

    решение HW5 12 *

--- mike_hw/05/helper.py
+++ mike_hw/05/helper.py
@@ -5,5 +5,21 @@ class Test:
         print(f"Test {self.n}: {msg}", 'OK' if ar == er else 'FAILED')
         self.n += 1
 
+    def expect(self, bool_expression, msg=''):
+        print(f"Test {self.n}: {msg}", 'OK' if bool_expression else 'FAILED')
+        self.n += 1
+
+    # def it(self, s):
+    #     def dec(function):
+    #         def wr(*args):
+    #             self.m = s
+    #             function(*args)
+    #             pass
+    #         return wr
+    #     return dec
+    #
+    # def run(self, dct):
+    #     [dct[k]() for k, v in dct.items() if "test_" in k]
+
 
 test = Test()
