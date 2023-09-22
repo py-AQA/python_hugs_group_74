27b21414db5a9f86c4290d84c97fba749b013be3
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-09-22 10:47:35 +0300

    решения HW7-HW9

---
+++ mike_hw/06/07 Filter the number_findall.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/55b051fac50a3292a9000025
+import re
+from helper import test
+
+
+def filter_string(string):
+    return int(''.join(re.findall("[0-9]", string)))
+    # return int(''.join(re.findall("\d", string)))
+
+
+# проверка
+test.assert_equals(filter_string("123"), 123, 'Just return the numbers')
+test.assert_equals(filter_string("a1b2c3"), 123, 'Just return the numbers')
+test.assert_equals(filter_string("aa1bb2cc3dd"), 123, 'Just return the numbers')
+test.assert_equals(filter_string("aa 112 3dd"), 1123, 'Just return the numbers')
+test.assert_equals(filter_string("11bb2c23c3"), 112233, 'Just return the numbers')
