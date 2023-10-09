9fc4870664cb56bc01b5c549f77e7f893376c4a1
Janek_Shalygin <61755149+IvShalygin@users.noreply.github.com>
2023-10-09 16:27:58 +0200

    add plan testing, first test

---
+++ HW/Jan_Sha/Silenium/HW_les_1/plan.md
@@ -0,0 +1,32 @@
+План что буду делать по "пощупать сайт" https://www.saucedemo.com
+
+Accepted usernames are:
+    standard_user
+    locked_out_user
+    problem_user
+    performance_glitch_user
+
+pass: secret_sauce
+
+1. Проверка на наличие полей на странице авторизации: 
+    + - user_name (Do have placeholder?)
+    + - password (Do have placeholder?)
+    + - button
+
+2. login verification: 
+    - войти под разными юзерами с валидными паролями
+    - logout system
+    - войти под разными юзерами с НЕ валидными паролями
+
+3. Checking:
+    - the existence of the number of blocks
+    - the existence of the number of image 
+    - the existence of the number of button
+    - the existence of a short description of products 
+    - the existence of a cost products
+
+4. Add anything products to recycled
+5. Remove added product from recycled
+6. Continue shopping
+7. First name, Second name, Zip_Code (valid or no?)
+8. Finish button
---
+++ HW/Jan_Sha/Silenium/HW_les_1/test_availabitity_field.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_availability_form_username():
+    # global results
+
+    # is there user_name
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    try:
+        username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    except:
+        results = False
+
+    if username_field:
+        results = True
+
+    assert results == True , 'The form don\'t have username_field'
+    driver.quit()
+
+def test_availability_form_username_placeholder():
+    # is there necessary  placeholder
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    username_field = driver.find_element(By.NAME, 'user-name')
+    placeholder = username_field.get_attribute('placeholder')
+
+    assert placeholder == "Username", 'The field don\'t have necessary  placeholder'
+    driver.quit()
+
+
+def test_availability_form_password():
+    # is there field_password
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    username_field = driver.find_element(By.ID, 'password')
+    if username_field:
+        results = True
+
+    assert results == True , 'The form don\'t have password_field'
+    driver.quit()
+
+def test_availability_form_password_placeholder():
+    # is there necessary  placeholder
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    username_field = driver.find_element(By.XPATH, '//input[@type="password"]')
+    placeholder = username_field.get_attribute('placeholder')
+
+    assert placeholder == "Password", 'The field don\'t have necessary  placeholder'
+    driver.quit()
+
+def test_availability_form_button_login():
+    # is there button_login
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    try:
+        username_field = driver.find_element(By.CLASS_NAME, 'submit-button')
+    except:
+        username_field = False
+
+    if username_field:
+        results = True
+
+    assert results == True , 'The form don\'t have field button LOGIN'
+    driver.quit()
+# <input type="submit" class="submit-button btn_action" data-test="login-button" id="login-button" name="login-button" value="Login">
+# driver.quit()
+
