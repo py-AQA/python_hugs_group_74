7627405b45536bc23c88bc4e5dbc3f899a98ebf5
Denis Stepulenok <super.denis@gmail.com>
2023-12-03 22:27:18 +0200

    [+] Mono + DMD compiler

--- docker/compilers/hw_pascal.pas
+++ docker/compilers/hw_pascal.pas
@@ -1,6 +1,6 @@
 var i: Integer;
 begin
-  WriteLn('Hello, World!');
+  WriteLn('== Программа на Pascal ==');
   Writeln('Привет всем!');
   for i := 2 to 7 do
     WriteLn('Квадрат числа ', i, ' равен ', i * i);
